83f2ee7dc363eaefbafdadd31bf66b36fe9d3e3e
Isabella <143600689+isabella960@users.noreply.github.com>
2023-10-23 05:00:40 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,43 +1,16 @@
-
-    for item in data:
-    return data
-    data = generate_random_data()
-if __name__ == "__main__":
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-
-import random
-
-def generate_random_data():
-        print(f"Random Number: {item}")
-
-    main()main();
-function main() {
-
     for (let i = 0; i < 10; i++) {
-    const data = generateRandomData();
+}
+    const data = [];
     }
-    return data;
-    data.forEach(item => {
 function generateRandomData() {
-
+    const data = generateRandomData();
+main();
     });
 
+    return data;
         console.log("Random Number: " + item);
-    const data = [];
-}
-}
+    data.forEach(item => {
         data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
 
-
-    for item in data:
-    return data
-if __name__ == "__main__":
-def generate_random_data():
-import random
-    data = generate_random_data()
-
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-    main()
-def main():
+}
